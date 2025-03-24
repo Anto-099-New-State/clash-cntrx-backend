da60a82875d0411510c4fc2efec7133134a79033
Anto-099-New-State <antoabisheik.25cs@licet.ac.in>
2025-03-24 17:57:18 +0530

    fixes

--- index.js
+++ index.js
@@ -1,23 +1,13 @@
-// app.js
 import express from "express";
 import trainRoutes from "./routes/train.js";
 import dotenv from "dotenv";
 import cors from "cors";
 
-
-
 dotenv.config();
-
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api", trainRoutes);
 
-export function startServer(port = 3000) {
-  app.listen(port, () => {
-    console.log(`🚀 Server running on port ${port}`);
-  });
-}
-startServer(3000);
 export default app;
